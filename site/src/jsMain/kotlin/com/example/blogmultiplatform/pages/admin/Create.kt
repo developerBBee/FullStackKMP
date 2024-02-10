e2510ae8bf123a6d1e97dced9a9427344953f4ef
BBee <atsushi.kokubun.47@gmail.com>
2024-02-10 13:31:00 +0900

    Create part of form to post

--- site/src/jsMain/kotlin/com/example/blogmultiplatform/pages/admin/Create.kt
+++ site/src/jsMain/kotlin/com/example/blogmultiplatform/pages/admin/Create.kt
@@ -1,10 +1,47 @@
 package com.example.blogmultiplatform.pages.admin
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.example.blogmultiplatform.components.AdminPageLayout
+import com.example.blogmultiplatform.models.Theme
+import com.example.blogmultiplatform.util.Constants
+import com.example.blogmultiplatform.util.Constants.FONT_FAMILY
 import com.example.blogmultiplatform.util.isUserLoggedIn
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
+import com.varabyte.kobweb.compose.ui.modifiers.border
+import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
+import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
+import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.height
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.outline
+import com.varabyte.kobweb.compose.ui.modifiers.padding
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.forms.Input
+import com.varabyte.kobweb.silk.components.forms.Switch
+import com.varabyte.kobweb.silk.components.forms.SwitchSize
+import com.varabyte.kobweb.silk.components.layout.SimpleGrid
+import com.varabyte.kobweb.silk.components.layout.numColumns
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
 import com.varabyte.kobweb.silk.components.text.SpanText
+import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
+import org.jetbrains.compose.web.attributes.InputType
+import org.jetbrains.compose.web.css.LineStyle
+import org.jetbrains.compose.web.css.px
 
 @Page
 @Composable
@@ -16,7 +53,148 @@ fun CreatePage() {
 
 @Composable
 fun CreateScreen() {
+    val breakpoint = rememberBreakpoint()
+    val isLarge: Boolean = (breakpoint > Breakpoint.MD)
+    var popularSwitch by remember { mutableStateOf(false) }
+    var mainSwitch by remember { mutableStateOf(false) }
+    var sponsoredSwitch by remember { mutableStateOf(false) }
+    var titleText by remember { mutableStateOf("") }
+    var subtitleText by remember { mutableStateOf("") }
+
     AdminPageLayout {
-        SpanText(text = "Create")
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    top = if (isLarge) 50.px else (50 + Constants.COLLAPSED_PANEL_HEIGHT).px,
+                    bottom = 50.px,
+                    left = if (isLarge) Constants.SIDE_PANEL_WIDTH.px else 0.px,
+                    right = 0.px,
+                ),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .maxWidth(700.px),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                SimpleGrid(numColumns = numColumns(base = 1, sm = 3)) {
+                    Row(
+                        modifier = Modifier
+                            .margin(
+                                right = if (breakpoint < Breakpoint.SM) 0.px else 24.px,
+                                bottom = if (breakpoint < Breakpoint.SM) 12.px else 0.px
+                            ),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Switch(
+                            modifier = Modifier.margin(right = 8.px),
+                            checked = popularSwitch,
+                            onCheckedChange = { popularSwitch = it },
+                            size = SwitchSize.LG
+                        )
+                        SpanText(
+                            modifier = Modifier
+                                .fontSize(14.px)
+                                .fontFamily(FONT_FAMILY)
+                                .color(Theme.HalfBlack.rgb),
+                            text = "Popular"
+                        )
+                    }
+                    Row(
+                        modifier = Modifier
+                            .margin(
+                                right = if (breakpoint < Breakpoint.SM) 0.px else 24.px,
+                                bottom = if (breakpoint < Breakpoint.SM) 12.px else 0.px
+                            ),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Switch(
+                            modifier = Modifier.margin(right = 8.px),
+                            checked = mainSwitch,
+                            onCheckedChange = { mainSwitch = it },
+                            size = SwitchSize.LG
+                        )
+                        SpanText(
+                            modifier = Modifier
+                                .fontSize(14.px)
+                                .fontFamily(FONT_FAMILY)
+                                .color(Theme.HalfBlack.rgb),
+                            text = "Main"
+                        )
+                    }
+                    Row(
+                        modifier = Modifier
+                            .margin(
+                                right = if (breakpoint < Breakpoint.SM) 0.px else 24.px,
+                                bottom = if (breakpoint < Breakpoint.SM) 12.px else 0.px
+                            ),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Switch(
+                            modifier = Modifier.margin(right = 8.px),
+                            checked = sponsoredSwitch,
+                            onCheckedChange = { sponsoredSwitch = it },
+                            size = SwitchSize.LG
+                        )
+                        SpanText(
+                            modifier = Modifier
+                                .fontSize(14.px)
+                                .fontFamily(FONT_FAMILY)
+                                .color(Theme.HalfBlack.rgb),
+                            text = "Sponsored"
+                        )
+                    }
+                }
+                Input(
+                    type = InputType.Text,
+                    value = titleText,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(54.px)
+                        .margin(topBottom = 12.px)
+                        .backgroundColor(Theme.LightGray.rgb)
+                        .borderRadius(r = 4.px)
+                        .border(
+                            width = 0.px,
+                            style = LineStyle.None,
+                            color = Colors.Transparent
+                        )
+                        .outline(
+                            width = 0.px,
+                            style = LineStyle.None,
+                            color = Colors.Transparent
+                        )
+                        .fontFamily(FONT_FAMILY),
+                    placeholder = "Title",
+                    onValueChanged = { titleText = it }
+                )
+                Input(
+                    type = InputType.Text,
+                    value = subtitleText,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(54.px)
+                        .margin(bottom = 12.px)
+                        .backgroundColor(Theme.LightGray.rgb)
+                        .borderRadius(r = 4.px)
+                        .border(
+                            width = 0.px,
+                            style = LineStyle.None,
+                            color = Colors.Transparent
+                        )
+                        .outline(
+                            width = 0.px,
+                            style = LineStyle.None,
+                            color = Colors.Transparent
+                        )
+                        .fontFamily(FONT_FAMILY),
+                    placeholder = "Subtitle",
+                    onValueChanged = { subtitleText = it }
+                )
+            }
+        }
     }
 }
