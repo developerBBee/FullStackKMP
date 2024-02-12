9b8b0db71c255e647cc2f95a5daa7aef5b2b60b1
BBee <atsushi.kokubun.47@gmail.com>
2024-02-12 11:04:28 +0900

    Create ApiListResponse

--- site/src/jsMain/kotlin/com/example/blogmultiplatform/models/Post.kt
+++ site/src/jsMain/kotlin/com/example/blogmultiplatform/models/Post.kt
@@ -16,3 +16,15 @@ actual data class Post(
     actual val main: Boolean = false,
     actual val sponsored: Boolean = false,
 )
+
+@Serializable
+actual data class PostWithoutDetails(
+    actual val _id: String = "",
+    actual val author: String,
+    actual val date: Long,
+    actual val title: String,
+    actual val subtitle: String,
+    actual val thumbnail: String,
+    actual val content: String,
+    actual val category: Category,
+)
